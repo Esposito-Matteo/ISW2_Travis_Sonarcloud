b92b701fdada7972ec3ddfac0dc997da2b4363ec
Matteo <test@tes>
2020-03-19 21:22:35 +0100

    Travis Commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Matteo
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
